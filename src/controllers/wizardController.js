b0b126b6283a2eb0e89bc66697add137a9152f4f
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-04-01 09:59:16 -0300

    feat: creat wizards CRUD

--- src/controllers/wizardController.js
+++ src/controllers/wizardController.js
@@ -25,10 +25,32 @@ const createWizard = async (req, res) => {
     try {
         const { name, house_id } = req.body;
         const newWizard = await wizardModel.createWizard(name, house_id);
-        res.status(201).json(newWizard);
+        res.status(201).json({ message: "Bruxo criado com sucesso!" });
     } catch (error) {
         res.status(500).json({ message: "Erro ao criar bruxo." });
     }
 };
 
-module.exports = { getAllWizards, getWizard, createWizard };
+const updateWizard = async (req, res) => {
+    try {
+        const { name, house_id } = req.body;
+        const updatedWizard = await wizardModel.updateWizard(req.params.id, name, house_id);
+        if (!updatedWizard) {
+            return res.status(404).json({ message: "Bruxo não encontrado." });
+        }
+        res.status(200).json({ message: "Bruxo atualizado com sucesso!" });
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao atualizar bruxo." });
+    }
+};
+
+const deleteWizard = async (req, res) => {
+    try {
+        const message = await wizardModel.deleteWizard(req.params.id);
+        res.status(200).json({ message: "Bruxo deletado com sucesso!" });
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao deletar bruxo." });
+    }
+};
+
+module.exports = { getAllWizards, getWizard, createWizard, updateWizard, deleteWizard };
