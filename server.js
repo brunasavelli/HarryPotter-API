3a3242779cdc818634640ba32ed6c00294974542
Bruna Savelli <bruna.savelli@aluno.senai>
2025-04-08 10:46:06 -0300

    feat: add filter

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", wizardRoutes);
+app.use("/api/wizards", wizardRoutes);
 app.use("/api", houseRoutes);
 app.use("/api", reportRoutes);
 
