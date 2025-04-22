6997e7e2ff53f3fd3cb2ca17dd58b72723376303
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-04-22 08:28:06 -0300

    feat: add apiKeyMiddleware

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const apiKeyMiddleware = require("./../config/apiKey");
 
 const reportController = require("./../controllers/reportController");
 
 //Rota para exportar CSV
+router.use(apiKeyMiddleware);
 router.get("/report/csv", reportController.exportWizardCSV);
 
 //Rota para o PDF
--- src/routes/wizardRoutes.js
+++ src/routes/wizardRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const wizardController = require("../controllers/wizardController");
 const upload = require("../config/upload"); 
+const apiKeyMiddleware = require("../config/apiKey");
 
+
+router.use(apiKeyMiddleware);
 /**
  * @swagger
  * tags:
